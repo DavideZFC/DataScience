ad9013cf76ba11f9d6cc914a6ba613d6d228472d
DavideZFC <davidemaranzfc@gmail.com>
2025-03-16 14:28:55 +0100

    ex_1

--- ex_1.py
+++ ex_1.py
@@ -1,25 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Carica il dataset (modifica il percorso con quello corretto)
+# Carica il dataset
 df = pd.read_csv("datasets\AB_NYC_2019.csv")
 df = df[df["price"] < 1000]
+print(df.info())
 
-mean_prices = df.groupby("neighbourhood_group")["price"].mean()
-std_prices = df.groupby("neighbourhood_group")["price"].std()
+df_by_host = df.groupby("host_id")['id'].count().sort_values(ascending=False)
+print(df_by_host.head())
 
-high_prices = df.groupby("neighbourhood_group")["price"].quantile(0.75)
-low_prices = df.groupby("neighbourhood_group")["price"].quantile(0.25)
-
-
-is_cheap = df["price"] < df["neighbourhood_group"].map(low_prices)
-is_medium = (df["price"] > df["neighbourhood_group"].map(low_prices)) & (df["price"] < df["neighbourhood_group"].map(high_prices))
-is_high = df["price"] > df["neighbourhood_group"].map(high_prices)
-
-rank = 1*is_medium + 2*is_high
-df['rank'] = rank
-
-rank_to_words = {0: "cheap", 1: "medium", 2: "expensive"}
-rank_name = df["rank"].map(rank_to_words)
-
-print(rank_name)
+host_id_to_name = df.set_index("neighbourhood")["host_name"].to_dict()
+print(host_id_to_name)
