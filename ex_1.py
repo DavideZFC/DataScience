60c73989deafd17610a5b96bfc3fa668bf3fac1f
DavideZFC <davidemaranzfc@gmail.com>
2025-03-17 21:32:43 +0100

    other exercises on .map()

--- ex_1.py
+++ ex_1.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 # Carica il dataset
 df = pd.read_csv("datasets\AB_NYC_2019.csv")
 df = df[df["price"] < 1000]
+df = df.dropna()
+
 print(df.info())
+new_df = df.set_index("neighbourhood")
+new_df = new_df["neighbourhood_group"].to_dict()
 
-df_by_host = df.groupby("host_id")['id'].count().sort_values(ascending=False)
-print(df_by_host.head())
-
-host_id_to_name = df.set_index("neighbourhood")["host_name"].to_dict()
-print(host_id_to_name)
+df["neighbourhood"] = df["neighbourhood"].map(new_df)
+print(df.iloc[0,:])
