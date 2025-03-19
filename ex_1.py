8ef55b13d67955d577de8d54f36f42a4af3e2f8e
DavideZFC <davidemaranzfc@gmail.com>
2025-03-19 11:29:11 +0100

    stuff

--- ex_1.py
+++ ex_1.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Carica il dataset
 df = pd.read_csv("datasets\AB_NYC_2019.csv")
 df = df[df["price"] < 1000]
-df = df.dropna()
+df = df.dropna(subset=["availability_365"])
 
 print(df.info())
-new_df = df.set_index("neighbourhood")
-new_df = new_df["neighbourhood_group"].to_dict()
 
-df["neighbourhood"] = df["neighbourhood"].map(new_df)
-print(df.iloc[0,:])
+recent_reviews = df[df["number_of_reviews"] > 5].sort_values(by="number_of_reviews", ascending=False)
+print(recent_reviews)
+
+reviews_per_neighbourhood = df.groupby("neighbourhood_group")["number_of_reviews"].sum()
+print(reviews_per_neighbourhood)
+
+reviews_per_neighbourhood.plot.bar()
+plt.show()
