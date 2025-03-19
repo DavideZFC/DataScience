21d161093f937db494f49e36a8e1f52b1a4972bb
DavideZFC <davidemaranzfc@gmail.com>
2025-03-19 13:37:25 +0100

    nana

--- ex_1.py
+++ ex_1.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Carica il dataset
 df = pd.read_csv("datasets\AB_NYC_2019.csv")
 df = df[df["price"] < 1000]
-df = df.dropna(subset=["availability_365"])
 
-print(df.info())
 
-recent_reviews = df[df["number_of_reviews"] > 5].sort_values(by="number_of_reviews", ascending=False)
-print(recent_reviews)
+pivo = df.pivot_table(values="price", index="host_id", columns="neighbourhood_group", aggfunc="mean")
 
-reviews_per_neighbourhood = df.groupby("neighbourhood_group")["number_of_reviews"].sum()
-print(reviews_per_neighbourhood)
+pivo.fillna(row.mean())
+host_sd = pivo.std(axis=1)
 
-reviews_per_neighbourhood.plot.bar()
-plt.show()
+# Create a DataFrame from the result
+host_sd_df = host_sd.reset_index().rename(columns={0: "host sd", "host_id": "host_id"})
+
+# Display result
+print(host_sd)
