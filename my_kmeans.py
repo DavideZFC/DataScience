2f2b2741aaa7781d0182370a3b34cae3cc04c8e9
DavideZFC <davidemaranzfc@gmail.com>
2025-03-23 22:16:18 +0100

    numba

--- my_kmeans.py
+++ my_kmeans.py
@@ -8,18 +8,17 @@ from numba import njit
 def find_k_smallest(distances, k):
     indices = np.full(k, -1)
     values = np.full(k, np.inf)
+    limit_distance = np.inf
+    worst_index = 0
     
     for i in range(len(distances)):
         d = distances[i]
-        max_idx = -1
-        max_val = -np.inf
-        for j in range(k):
-            if values[j] > max_val:
-                max_val = values[j]
-                max_idx = j
-        if d < max_val:
-            values[max_idx] = d
-            indices[max_idx] = i
+        if d < limit_distance:
+            indices[worst_index] = i
+            values[worst_index] = d
+
+            worst_index = np.argmax(values)
+            limit_distance = values[worst_index]
     return values, indices
 
 
